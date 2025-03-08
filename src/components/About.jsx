755a9e21ca7b034a6b61ef7ed762e1ac6353caa1
Muhammad Nouman Arshad <muhammadnoumanarshad013@gmail.com>
2025-03-08 19:34:35 +0500

    3rdChange

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,28 +3,38 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <div 
-      name="About"
-      className='max-w-screen-2xl container mx-auto px-4 md:px-20 py-14'>
+      <div name="About" className='max-w-screen-2xl container mx-auto px-4 md:px-20 py-14'>
 
         <h1 className='font-bold text-3xl'>About</h1>
-        <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quam incidunt unde nobis modi necessitatibus dolores repellat velit, voluptate harum soluta possimus, inventore optio laboriosam architecto sed accusamus eveniet minus.</p>
+        <p className='text-justify'>Hello, I'm Muhammad Nouman Arshad, a passionate Frontend Developer with skills in HTML, CSS, JavaScript, React, Figma, and GitHub. As a fresher with a strong portfolio, I am eager to bring innovative ideas to the frontend development world. I'm currently seeking a frontend developer role to apply my skills and grow professionally.</p>
         <br />
 
         <h1 className='font-bold text-2xl'>Education & Training</h1>
-        <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi tenetur amet pariatur architecto quibusdam rerum totam, ipsum, est nobis obcaecati alias ipsam. Harum, facilis. Nobis suscipit facilis dolorum totam!</p>
+        <div className='text-justify mt-1.5'>
+          <p className='font-bold md:px-11 text-lg'>•	Bachelors in Computer Science - 2025</p>
+          <p className='md:px-22'>University of the Punjab, Lahore, Pakistan</p>
+          <p className='font-bold md:px-11 text-lg'>•	Intermediate/HSSC	- 2021</p>
+          <p className='md:px-22'>Punjab Group of College, Lahore</p>
+          <p className='font-bold md:px-11 text-lg'>•	Matriculation/SSC	- 2019</p>
+          <p className='md:px-22'>Sadiq Memorial High School, Khudian Khas</p>          	
+        </div>
         <br />
 
-        <h1 className='font-bold text-2xl'>Professional Experience</h1>
-        <p className='text-justify'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis sequi impedit iusto magnam incidunt ea illo libero quasi laudantium soluta id delectus iure, cupiditate cum autem dolor rerum obcaecati accusantium!</p>
+        {/* <h1 className='font-bold text-2xl'>Professional Experience</h1>
+        <p className='text-justify'>[Freelance/Contract Work], [Client/Organization], [Dates]
+          [Brief description of projects and contributions] 
+        </p>
         <br />
 
         <h1 className='font-bold text-2xl'>Achievements & Awards</h1>
-        <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam temporibus, non voluptatem itaque ipsam modi quasi natus nisi mollitia ipsa, dolor eum nulla iste id enim laboriosam! Libero, autem minima.</p>
-        <br />
+        <p className='text-justify'>[Achievement], [Organization/Institution], [Year]
+        Finalist in Winter League | "Final Lost in Winter League", [Year]
+        </p>
+        <br /> */}
 
         <h1 className='font-bold text-2xl'>Mission Statement</h1>
-        <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloremque sed tempora impedit consequuntur. Totam error laborum odit animi id maxime praesentium ab. Explicabo porro commodi blanditiis sit quibusdam eligendi.</p>
+        <p className='text-justify'>My mission is to leverage my frontend development skills and creativity to deliver user-friendly and visually appealing web solutions. I am committed to continuous learning and excited to take on new challenges while contributing to dynamic projects in the tech landscape.
+        </p>
 
       </div>
     </div>
